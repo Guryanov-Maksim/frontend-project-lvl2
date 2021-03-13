4b6320ae78fb8b4811a0ca3a9f157b1d512e5a45
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-03-13 15:44:36 +0300

    feat: specify the expected command-arguments for gendiff
    rus: указывает ожидаемые аргументы для gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,7 @@ import { program } from 'commander';
 
 program
   .description('Compares two configuration files and shows a difference.')
-  .version('1.0.0', '-V, --version', 'output the version number');
+  .version('1.0.0', '-V, --version', 'output the version number')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse();
