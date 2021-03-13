7f3e098df0842fa84f3712c012fc4d91df952094
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-03-13 19:35:48 +0300

    feat: add computation of difference between 2 plane JSON files
    rus: добавляет вычисление разницы между 2-мя плоскими JSON файлами

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,16 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import getDifference from '../lib/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
+  .option('-f, --format [type]', 'output format')
+  .action((filepath1, filepath2) => {
+    const difference = getDifference(filepath1, filepath2);
+    console.log(difference);
+  });
 
 program.parse();
