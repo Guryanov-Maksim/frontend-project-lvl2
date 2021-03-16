b9fe407d8fe92f419c4aa65000f9c5328fca3e2b
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-03-16 13:44:50 +0300

    add tests
    rus: добавляет тесты

--- Makefile
+++ Makefile
@@ -1,2 +1,4 @@
 gendiff:
 	node bin/gendiff.js
+test:
+	npx jest
---
+++ __tests__/index.test.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@jest/globals';
+import { fileURLToPath } from 'url';
+import path from 'path';
+import fs from 'fs';
+import getDifference from '../src/index.js';
+
+const { dirname } = path;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
+test('compare two JSON-files', () => {
+  const beforePath = getFixturePath('before.json');
+  const afterPath = getFixturePath('after.json');
+  const expectedResult = readFile('result.txt');
+  const result = getDifference(beforePath, afterPath);
+
+  expect(result).toEqual(expectedResult);
+});
