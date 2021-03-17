022e03994c3c4e3ff5518219bb6736318b9eb802
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-03-17 10:18:32 +0300

    refactor: remove duplication
    rus: удаляет дублировние

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,4 +1,4 @@
-import { test, expect } from '@jest/globals';
+import { test, expect, beforeAll } from '@jest/globals';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import fs from 'fs';
@@ -10,10 +10,15 @@ const __dirname = dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
+let expectedResult;
+
+beforeAll(() => {
+  expectedResult = readFile('result.txt');
+});
+
 test('compare two JSON-files', () => {
   const beforePath = getFixturePath('before.json');
   const afterPath = getFixturePath('after.json');
-  const expectedResult = readFile('result.txt');
   const result = getDifference(beforePath, afterPath);
 
   expect(result).toEqual(expectedResult);
@@ -22,7 +27,6 @@ test('compare two JSON-files', () => {
 test('compare two YML-files', () => {
   const beforePath = getFixturePath('before.yml');
   const afterPath = getFixturePath('after.yml');
-  const expectedResult = readFile('result.txt');
   const result = getDifference(beforePath, afterPath);
 
   expect(result).toEqual(expectedResult);
