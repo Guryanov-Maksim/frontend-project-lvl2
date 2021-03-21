c2b03fdc8bf6909cd032f44c8796d7dbc34ab462
Maksim Guryanov <guryanov-mv@yandex.ru>
2021-03-21 19:15:41 +0300

    fix: wrong path in import statement
    rus: исправляет неправильный путь в инструкции import

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
-import getDifference from '../src/index.js';
+import getDifference from '../lib/index.js';
 
 program
   .description('Compares two configuration files and shows a difference.')
