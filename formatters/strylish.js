6547f87f31064abe3e458f55fbb4bf037ef5f4cf
Maksim Guryanov <guryanov-mv@yandex.ru>
2021-03-21 22:14:16 +0300

    refactor: change stylish because of changing diff tree
    rus: изменяет форматтер stylish из-за изменения дерева диффов

--- formatters/strylish.js
+++ formatters/strylish.js
@@ -1,21 +1,44 @@
 import _ from 'lodash';
 
+const hasChildren = (tree) => Array.isArray(tree);
+
+const makeDiffItem = (item) => {
+  const addSign = '+';
+  const removeSign = '-';
+  const unchangeSign = '  ';
+  const { key, type, value } = item;
+  if (type === 'added') {
+    return `${addSign} ${key}: ${value}`;
+  }
+  if (type === 'removed') {
+    return `${removeSign} ${key}: ${value}`;
+  }
+  if (type === 'updated') {
+    const { valueBefore, valueAfter } = value;
+    return `${addSign} ${key}: ${valueBefore}\n${removeSign} ${key}: ${valueAfter}`;
+  }
+  return `${unchangeSign} ${key}: ${value}`;
+};
+
 export default (object) => {
   const space = ' ';
   const spaceCount = 4;
-  const inner = (obj, depth) => {
-    const keys = Object.keys(obj);
+  const inner = (tree, depth) => {
     const entryUndent = space.repeat(depth * spaceCount);
     const endUndent = space.repeat((depth - 1) * spaceCount);
-    const result = keys.map((key) => {
-      const value = obj[key];
-      const undent = key.startsWith('+') || key.startsWith('-')
-        ? entryUndent.slice(0, entryUndent.length - 2)
-        : entryUndent;
-      if (_.isObjectLike(value)) {
-        return `${undent}${key}: ${inner(value, depth + 1)}`;
-      }
-      return `${undent}${key}: ${value}`;
+    if (!hasChildren(tree)) {
+      return `${entryUndent}${makeDiffItem(tree)}`;
+    }
+    const { children } = tree;
+    const result = children.map((child) => {
+      // const undent = key.startsWith('+') || key.startsWith('-')
+      //   ? entryUndent.slice(0, entryUndent.length - 2)
+      //   : entryUndent;
+      // if (_.isObjectLike(value)) {
+      const item = inner(child, depth + 1);
+      return `${entryUndent}${makeDiffItem(item)}`;
+      // }
+      // return `${undent}${key}: ${value}`;
     });
     return `{\n${result.join('\n')}\n${endUndent}}`;
   };
