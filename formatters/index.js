30169058084d429b3e6410b8822abc831610cc18
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-03-29 18:36:35 +0300

    feat: change default behavior of parser
    rus: изменяет поведение по умолчанию парсера

--- formatters/index.js
+++ formatters/index.js
@@ -2,13 +2,15 @@ import plain from './plain.js';
 import stylish from './strylish.js';
 import makeJson from './json.js';
 
-export default (obj, formatterName) => {
+export default (obj, formatterName = 'stylish') => {
   switch (formatterName) {
     case 'json':
       return JSON.stringify(makeJson(obj), null, '  ');
     case 'plain':
       return plain(obj);
-    default:
+    case 'stylish':
       return stylish(obj);
+    default:
+      throw new Error('non supported format');
   }
 };
