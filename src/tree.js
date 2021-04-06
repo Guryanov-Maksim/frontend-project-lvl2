7fe8e572758971222daede38b61c6eb916ac9071
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-04-06 14:52:05 +0300

    refactor: remove destructuring
    rus: удаляет декструктуризацию

--- src/tree.js
+++ src/tree.js
@@ -1,14 +1,6 @@
 import _ from 'lodash';
 import types from './types.js';
 
-const {
-  added,
-  removed,
-  nested,
-  unchanged,
-  updated,
-} = types;
-
 const makeDiffTree = (obj1, obj2) => {
   const keys = Object.keys({ ...obj1, ...obj2 });
   const sortedKeys = _.sortBy(keys);
@@ -16,25 +8,25 @@ const makeDiffTree = (obj1, obj2) => {
     const valueBefore = _.get(obj1, key);
     const valueAfter = _.get(obj2, key);
     if (!_.has(obj1, key)) {
-      return { key, type: added, values: { valueAfter } };
+      return { key, type: types.added, values: { valueAfter } };
     }
     if (!_.has(obj2, key)) {
-      return { key, type: removed, values: { valueBefore } };
+      return { key, type: types.removed, values: { valueBefore } };
     }
     if (_.isPlainObject(valueAfter) && _.isPlainObject(valueBefore)) {
-      return { key, type: nested, children: makeDiffTree(obj1[key], obj2[key]) };
+      return { key, type: types.nested, children: makeDiffTree(obj1[key], obj2[key]) };
     }
     if (valueBefore !== valueAfter) {
       return {
         key,
-        type: updated,
+        type: types.updated,
         values: {
           valueBefore,
           valueAfter,
         },
       };
     }
-    return { key, type: unchanged, values: { valueBefore } };
+    return { key, type: types.unchanged, values: { valueBefore } };
   });
   return result;
 };
