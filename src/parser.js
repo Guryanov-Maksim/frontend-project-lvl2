d1e34be4ab3b99b651e44e072103725384d8f045
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-04-06 15:19:47 +0300

    refactor: corrent naming
    rus: исправляет наименования

--- src/parser.js
+++ src/parser.js
@@ -1,12 +1,12 @@
 import yaml from 'js-yaml';
 
-export default (fileExtention, data) => {
-  switch (fileExtention) {
+export default (dataFormat, data) => {
+  switch (dataFormat) {
     case '.yml':
       return yaml.load(data);
     case '.json':
       return JSON.parse(data);
     default:
-      throw new Error(`non supported data format: ${fileExtention}`);
+      throw new Error(`non supported data format: ${dataFormat}`);
   }
 };
