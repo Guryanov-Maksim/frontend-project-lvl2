a1fda3f856e402bc59ed1471c1db5a2cd358b149
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-04-13 11:43:40 +0300

    refactor: update parser
    rus: обновляет парсер

--- src/parser.js
+++ src/parser.js
@@ -1,12 +1,16 @@
 import yaml from 'js-yaml';
 
 export default (dataFormat, data) => {
-  switch (dataFormat) {
-    case '.yml':
+  const fileType = dataFormat
+    .split('')
+    .filter((letter) => letter !== '.')
+    .join('');
+  switch (fileType) {
+    case 'yml':
       return yaml.load(data);
-    case '.json':
+    case 'json':
       return JSON.parse(data);
     default:
-      throw new Error(`non supported data format: ${dataFormat}`);
+      throw new Error(`non supported data format: ${fileType}`);
   }
 };
