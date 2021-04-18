4f7a6fe96fb91b1820951f482ab84cb9e1cf7949
Guryanov-Maksim <guryanov-mv@yandex.ru>
2021-04-18 11:59:22 +0300

    refactor: join test.eachs, simplify tests
    rus: объединяет test.each-и, упрощает тесты

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,37 +1,35 @@
-import _ from 'lodash';
 import { test, expect } from '@jest/globals';
-import { fileURLToPath } from 'url';
+import url from 'url';
 import path from 'path';
 import fs from 'fs';
 import getDifference from '../src/index.js';
 
-const fileTypes = ['json', 'yml'];
-const formats = ['stylish', 'plain', 'json'];
-
-const { dirname } = path;
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __filename = url.fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
-const resultsForEachFormat = formats.map((format) => readFile(`result_${format}.txt`));
-const expectedResults = Object.fromEntries(_.zip(formats, resultsForEachFormat));
 
-test.each(fileTypes)('compare two %s files', (extention) => {
-  const pathToBefore = getFixturePath(`before.${extention}`);
-  const pathToAfter = getFixturePath(`after.${extention}`);
-  const result = getDifference(pathToBefore, pathToAfter);
-  expect(result).toEqual(expectedResults.stylish);
-});
+const formats = ['stylish', 'plain', 'json'];
+const expectedResults = Object.fromEntries(
+  formats.map((format) => [format, readFile(`result_${format}.txt`)]),
+);
 
-test.each(formats)('output the difference in various formats (%s format)', (format) => {
-  const pathToBefore = getFixturePath('before.json');
-  const pathToAfter = getFixturePath('after.json');
+test.each([
+  ['yml', 'stylish'],
+  ['json', 'stylish'],
+  ['json', 'plain'],
+  ['json', 'json'],
+])('compare two %s files and output in %s format', (dataFormat, format) => {
+  const pathToBefore = getFixturePath(`before.${dataFormat}`);
+  const pathToAfter = getFixturePath(`after.${dataFormat}`);
   const result = getDifference(pathToBefore, pathToAfter, format);
   expect(result).toEqual(expectedResults[format]);
 });
 
-test('non supported formatter', () => {
+test('default behavior', () => {
   const pathToBefore = getFixturePath('before.json');
   const pathToAfter = getFixturePath('after.json');
+  const result = getDifference(pathToBefore, pathToAfter);
+  expect(result).toEqual(expectedResults.stylish);
   expect(() => getDifference(pathToBefore, pathToAfter, 'wrong')).toThrow();
 });
